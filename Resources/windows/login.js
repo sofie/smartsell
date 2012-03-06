e370e5b711167432523f82c5b56e9ffe10c215db
Sofie Hendrickx <sofie.hendrickx@hotmail.com>
2012-03-06 16:17:56 +0100

    Login naar mainWin + logout

--- Resources/windows/login.js
+++ Resources/windows/login.js
@@ -1,114 +1,87 @@
 (function() {
-	var updateTimeout = 15000;
-	var i = 0;
-	var navWindow;   	
-   	
-//
-    //
-   /*var tabGroup = Titanium.UI.createTabGroup();  
-   	//nieuw
-	var main    = Titanium.UI.createWindow();
-	//nieuw
-	var mainTab = Titanium.UI.createTab();*/
-	//
-	
+	var navWindow;
 
-	var win = Titanium.UI.createWindow({  
-	    title:'Inloggen',  
-	    barImage : 'img/header.png',
-			fullscreen : false,
-			font : {
-				fontFamily : 'Bree Serif'
-			},
-	    tabBarHidden:true,  
-	    //url:'main_windows/login.js'  
-	    
-	});  
-	
-	/*var loginTab = Titanium.UI.createTab({  
-	    title:"Login",  
-	    window:win //login  
-	}); */ 
-	
-	 // 
-	/*tabGroup.addTab(loginTab);  
-	tabGroup.open();*/
-	//
-	
+	var loginWin = Titanium.UI.createWindow({
+		barImage : 'img/header.png',
+		fullscreen : false,
+		tabBarHidden : true,
+		modal : true,
+		backgroundImage:'img/bg.png'
+	});
+
+	var lblTitle = Titanium.UI.createLabel({
+		text : 'Inloggen',
+		color : '#fff',
+		font : {
+			fontFamily : 'Bree Serif',
+			fontSize : 24
+		}
+	});
+	loginWin.setTitleControl(lblTitle);
+
 	//Eerste scherm
 	navWindow = Ti.UI.createWindow();
 	Smart.navGroup = Ti.UI.iPhone.createNavigationGroup({
-		window : win
+		window : loginWin
 	});
 	navWindow.add(Smart.navGroup);
 
 	navWindow.open({
 		transition : Ti.UI.iPhone.AnimationStyle.CURL_DOWN
 	});
+	
+	//
+	//Inloggen via scannen
+	//
+	var labelPersoneelskaart = Titanium.UI.createLabel({
+		color : '#474240',
+		text : 'Scan de barcode van uw personeelskaart om in te loggen.',
+		left : 20,
+		top : -350,
+		font : {
+			fontSize : 15,
+			fontFamily : 'Bree Serif',
+		},
+		textAlign : 'left',
+		width : '280'
 
-   	
-   	//
-	  
-   	
-   	//var win = Titanium.UI.currentWindow;  
-      
-      //label
- 	   var labelPersoneelskaart = Titanium.UI.createLabel({
-    	color:'#888',
-		text:'Scan de barcode van uw personeelskaart om in te loggen.',
-		left: 10,
-		top:-350,
-		font:{
-			fontSize:15,
-			fontFamily:'Bree Serif'
-			},
-		textAlign:'left',
-		width:'280'
-    	
-    })
-    
-    win.add(labelPersoneelskaart);
-    
-    //foto personeelskaart
-    var afbPersoneelskaart = Titanium.UI.createImageView({
-    	image:"/img/personeelskaart.png",
-    	left:35,
-    	top: 70,
-    	width:247,
-    	height:181
-    	
-    	
-    })
-    
-    win.add(afbPersoneelskaart); 
-    
-    
-    //label
-    var labelPersoneelsnummer = Titanium.UI.createLabel({
-    	color:'#888',
-		text:'Inloggen via scannen lukt niet? Log hier in.',
-		left: 10,
-		top:180,
-		font:{
-			fontSize:15,
-			fontFamily:'Bree Serif'
-			},
-		textAlign:'left',
-		width:'300'
-    	
-    })
-    
-    win.add(labelPersoneelsnummer);
-    
-    
-    //inputveld
-    var personeelNummer = Titanium.UI.createTextField({  
-        color : '#888',  
-        top:320,  
-        left:10,  
-        width:300,  
-        height:40,  
-        //width : widthTxtField,
+	})
+	loginWin.add(labelPersoneelskaart);
+
+	var afbPersoneelskaart = Titanium.UI.createImageView({
+		image : "/img/personeelskaart.png",
+		left : 'auto',
+		right : 'auto',
+		top : 70,
+		width : 247,
+		height : 181
+	})
+	loginWin.add(afbPersoneelskaart);
+
+	//
+	//Inloggen via personeelsnummer
+	//
+	var labelPersoneelsnummer = Titanium.UI.createLabel({
+		color : '#474240',
+		text : 'Inloggen via scannen lukt niet? Log hier in.',
+		left : 20,
+		top : 180,
+		font : {
+			fontSize : 15,
+			fontFamily : 'Bree Serif'
+		},
+		textAlign : 'left',
+		width : '300'
+	})
+	loginWin.add(labelPersoneelsnummer);
+
+	var widthTxtField = Titanium.Platform.displayCaps.platformWidth - 40;
+	var personeelNummer = Titanium.UI.createTextField({
+		color : '#474240',
+		top : 320,
+		left : 20,
+		height : 40,
+		width : widthTxtField,
 		height : 40,
 		hintText : 'Personeelsnummer',
 		font : {
@@ -116,96 +89,66 @@
 			fontFamily : 'Bree Serif'
 		},
 		opacity : 0.65,
-		keyboardType : Titanium.UI.KEYBOARD_DEFAULT,
+		keyboardType : Titanium.UI.KEYBOARD_NUMBERS_PUNCTUATION,
 		returnKeyType : Titanium.UI.RETURNKEY_DEFAULT,
 		borderStyle : Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
-		clearButtonMode : Titanium.UI.INPUT_BUTTONMODE_ALWAYS  
-    });  
-    win.add(personeelNummer);  
-      
-    //loginbutton
-    var loginBtn = Titanium.UI.createButton({
-    	backgroundImage : '/img/btn_inloggen.png',  
-        title:'Inloggen',  
-        top:370,
-        right:10,  
-        width:90,  
-        height:35,  
-        borderRadius:1,  
-        font:{
-        	fontFamily:'Bree Serif',
-        	fontWeight:'bold',
-        	fontSize:14}  
-    });  
-    win.add(loginBtn);  
-    
-    
-    
-    
-    
-    
+		clearButtonMode : Titanium.UI.INPUT_BUTTONMODE_ALWAYS
+	});
+	loginWin.add(personeelNummer);
+
+	var loginBtn = Titanium.UI.createButton({
+		backgroundImage : '/img/btn_inloggen.png',
+		title : 'Inloggen',
+		top : 370,
+		right : 20,
+		width : 90,
+		height : 35,
+		font : {
+			fontFamily : 'Bree Serif',
+			fontWeight : 'bold',
+			fontSize : 14
+		}
+	});
+	loginWin.add(loginBtn);
+	
+	//
+	//Login Service
+	//
+	
 	//request
-	var loginReq = Titanium.Network.createHTTPClient();  
-  
-  
-    //json response
-    loginReq.onload = function()  
-    {  
-        var json = this.responseText;  
-        var response = JSON.parse(json);  
-        if (response.logged == true)  
-        {  
-           alert("Welcome " + response.personeelNummer + ".");  
-            /*personeelNummer.blur();   
-	        Ti.App.fireEvent('grantEntrance', {  
-           	 	personeelNummer:response.personeelNummer  
-        	}); 	 
-       	 	win.close();*/
-        }  
-        else  
-        {  
-            alert(response.message);  
-        }  
-    };  
-  
-  
-  	//verbinding met phpfile en database
-	loginBtn.addEventListener('click',function(e)  
-	{  
-	    if (personeelNummer.value != '')  
-	    {  
-	        loginReq.open("POST","http://localhost/AuthSmartsell/post_auth.php");  //aanpassen!!!
-	        var params = {  
-	            personeelNummer: personeelNummer.value,   
-	        };  
-	        loginReq.send(params);  
-	    }  
-	    else  
-	    {  
-	        alert("Gelieve uw personeelskaart te scannen of uw personeelsnummer in te geven.");  
-	    }  
+	var loginReq = Titanium.Network.createHTTPClient();
+
+	//json response
+	loginReq.onload = function() {
+		var json = this.responseText;
+		var response = JSON.parse(json);
+		if(response.logged == true) {
+			loginWin.close();
+			mainWin = Smart.ui.createApplicationMainWin();
+			mainWin.open();
+
+		} else {
+			alert(response.message);
+		}
+	};
+	//verbinding met phpfile en database
+	loginBtn.addEventListener('click', function() {
+		if(personeelNummer.value != '') {
+			loginReq.open("POST", "http://localhost/AuthSmartsell/post_auth.php");
+			var params = {
+				personeelNummer : personeelNummer.value,
+			};
+			loginReq.send(params);
+		} else {
+			alert("Gelieve uw personeelskaart te scannen of uw personeelsnummer in te geven.");
+		}
 	});
 	
-	
-	
-	//Login ok?: doorsturen naar koppelingspagina
-
-    Ti.App.addEventListener('grantEntrance', function(event)  
-    {  
-         //main.tabBarHidden   = true;  
-         //main.title      = 'Welkom ' + event.personeelNummer;  
-         //main.url        = 'windows/main.js';
-         //main.personeelNummer      = event.personeelNummer;  
-         //mainTab.window      = main;  
-       
-          /*tabGroup.addTab(mainTab);  
-          tabGroup.removeTab(loginTab);*/ 
-    });  
-  
-	
-	
-	
-	
-
-//
+	//
+	//Logout
+	//
+	Titanium.App.addEventListener('app:logout', function(e) {
+		personeelNummer.value = '';
+		loginWin.open();
+	});
 })();
--- Resources/windows/main.js
+++ Resources/windows/main.js
@@ -1,83 +1,99 @@
-/*(function() {
-	var updateTimeout = 15000;
-	var i = 0;
-	var navWindow;*/
+(function() {
+	var navWindow;
 
-	var mainWindow = Titanium.UI.createWindow({
-		barImage : 'img/header.png',
-		fullscreen : false,
-		font : {
-			fontFamily : 'Bree Serif'
-		}
-	});
-	
-	/*navWindow = Ti.UI.createWindow();
-	Smart.navGroup = Ti.UI.iPhone.createNavigationGroup({
-		window : mainWindow
-	});
-	navWindow.add(Smart.navGroup);
+	Smart.ui.createApplicationMainWin = function() {
+		var mainWindow = Titanium.UI.createWindow({
+			barImage : 'img/header.png',
+			fullscreen : false,
+			font : {
+				fontFamily : 'Bree Serif'
+			}
+		});
+		navWindow = Ti.UI.createWindow();
 
-	navWindow.open({
-		transition : Ti.UI.iPhone.AnimationStyle.CURL_DOWN
-	});*/
-	
-	//
-	//Navigationbar
-	//
-	var lblTitle = Titanium.UI.createLabel({
-		text : 'Koppelingen',
-		color : '#fff',
-		font : {
-			fontFamily : 'Bree Serif',
-			fontSize : 24
-		}
-	});
-	mainWindow.setTitleControl(lblTitle);
+		Smart.navGroup = Ti.UI.iPhone.createNavigationGroup({
+			window : mainWindow
+		});
+		navWindow.add(Smart.navGroup);
 
-	var addButton = Titanium.UI.createButton({
-		backgroundImage : "img/btn_add.png",
-		width : 37,
-		height : 35
-	});
-	addButton.addEventListener('click', function() {
+		navWindow.open();
 
-	});
-	mainWindow.rightNavButton = addButton;
+		//
+		//Navigationbar
+		//
+		var lblTitle = Titanium.UI.createLabel({
+			text : 'Koppelingen',
+			color : '#fff',
+			font : {
+				fontFamily : 'Bree Serif',
+				fontSize : 24
+			}
+		});
+		mainWindow.setTitleControl(lblTitle);
 
-	//
-	//Zoek bestaande koppeling
-	//
-	var widthTxtField = Titanium.Platform.displayCaps.platformWidth - 43 - 45;
+		var addButton = Titanium.UI.createButton({
+			backgroundImage : "img/btn_add.png",
+			width : 37,
+			height : 35
+		});
+		addButton.addEventListener('click', function() {
 
-	var searchField = Titanium.UI.createTextField({
-		color : '#888',
-		top : 20,
-		left : 20,
-		width : widthTxtField,
-		height : 40,
-		hintText : 'Zoek bestaande koppeling...',
-		font : {
-			fontSize : 15,
-			fontFamily : 'Bree Serif'
-		},
-		opacity : 0.65,
-		keyboardType : Titanium.UI.KEYBOARD_DEFAULT,
-		returnKeyType : Titanium.UI.RETURNKEY_DEFAULT,
-		borderStyle : Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
-		clearButtonMode : Titanium.UI.INPUT_BUTTONMODE_ALWAYS
-	});
+		});
+		mainWindow.rightNavButton = addButton;
 
-	var btnSearch = Titanium.UI.createButton({
-		backgroundImage : 'img/btn_search.png',
-		width : 43,
-		height : 42,
-		right : 20,
-		top : 20
-	});
-	btnSearch.addEventListener('click', function(e) {
-		Ti.API.info('Zoek koppeling: ' + searchField.value)
-	});
-	mainWindow.add(searchField);
-	mainWindow.add(btnSearch);
+		//
+		//Zoek bestaande koppeling
+		//
+		var widthTxtField = Titanium.Platform.displayCaps.platformWidth - 43 - 45;
 
-/*})();*/
+		var searchField = Titanium.UI.createTextField({
+			color : '#888',
+			top : 20,
+			left : 20,
+			width : widthTxtField,
+			height : 40,
+			hintText : 'Zoek bestaande koppeling...',
+			font : {
+				fontSize : 15,
+				fontFamily : 'Bree Serif'
+			},
+			opacity : 0.65,
+			keyboardType : Titanium.UI.KEYBOARD_DEFAULT,
+			returnKeyType : Titanium.UI.RETURNKEY_DEFAULT,
+			borderStyle : Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+			clearButtonMode : Titanium.UI.INPUT_BUTTONMODE_ALWAYS
+		});
+
+		var btnSearch = Titanium.UI.createButton({
+			backgroundImage : 'img/btn_search.png',
+			width : 43,
+			height : 42,
+			right : 20,
+			top : 20
+		});
+		btnSearch.addEventListener('click', function(e) {
+			Ti.API.info('Zoek koppeling: ' + searchField.value)
+		});
+		mainWindow.add(searchField);
+		mainWindow.add(btnSearch);
+
+		//
+		//Logout
+		//
+		var logoutButton = Titanium.UI.createButton({
+			backgroundImage : 'img/btn_logout.png',
+			height : 42,
+			width : 303,
+			bottom : 20,
+			left : 'auto',
+			right : 'auto'
+		});
+		mainWindow.add(logoutButton);
+		logoutButton.addEventListener('click', function() {
+			mainWindow.close();
+			Ti.App.fireEvent('app:logout', {
+				action : 'Logout klik'
+			});
+		});
+	}
+})();
